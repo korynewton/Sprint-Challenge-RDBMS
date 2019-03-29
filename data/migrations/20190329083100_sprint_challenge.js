b7579f1c2fe7ebab2ff8b2646fde50d5e4a782f1
korynewton <korynewtondev@gmail.com>
2019-03-29 08:42:19 -0700

    migration set up

---
+++ data/migrations/20190329083100_sprint_challenge.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('projects', function(tbl){
+        tbl.increments();
+        tbl.string('name', 255).notNullable().unique();
+        tbl.string('description', 255).notNullable().unique();
+        tbl.boolean('completed').notNullable();
+
+    })
+
+    .createTable('actions', function(tbl){
+        tbl.increments();
+        tbl.string('description', 255).notNullable().unique();
+        tbl.string('notes', 255).notNullable().unique();
+        tbl.boolean('completed').notNullable();
+        tbl.integer('project_id').unsigned().references('id').inTable('projects')
+    })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const server = express()
 
+server.use(express.json())
+
 server.listen(4000, () => {
     console.log("\n** server up and running on prt 4k**")
 })
